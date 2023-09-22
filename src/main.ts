6a1f7721e3bb7dfdd70530ba9f27945204f3b893
mufeiyu-ayu <2382839439@qq.com>
2023-09-22 14:09:08 +0800

    异常拦截器

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { Request, Response, NextFunction } from 'express';
 import * as cors from 'cors';
 import { join } from 'path';
 import { Response as response } from './common/response';
+import { HttpFilter } from './common/filter';
 // 设置白名单，只有跳转cat的才能正常跳转，其余一律返回指定send
 const whiteList = ['/cat'];
 function MiddleWareAll(req: Request, res: Response, next: NextFunction) {
@@ -29,7 +30,8 @@ async function bootstrap() {
       cookie: { maxAge: 1000000 },
     })
   );
-  app.useGlobalInterceptors(new response());
+  app.useGlobalFilters(new HttpFilter()); // 异常拦截器
+  app.useGlobalInterceptors(new response()); // 响应拦截器
   // app.use(cors); // 第三方解决跨域的中间件
   // app.use(MiddleWareAll); // 使用全局中间件
   await app.listen(3000);
